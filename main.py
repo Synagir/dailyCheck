d27e40bf2f14b98835b370342846f8528dba0b89
jin-young-lee <jin-young-lee@nts-corp.com>
2023-04-28 14:51:15 +0900

    add Star Rail Daily Check actions

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import os
+ 
+ltuid = os.environ.get('LTUID')
+ltoken = os.environ.get('LTOKEN')
+ 
+url = "https://sg-public-api.hoyolab.com/event/luna/os/sign?act_id=e202303301540311&lang=ko-kr"
+ 
+headers = {
+        "Referer": "https://act.hoyolab.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+    }
+ 
+cookies = {'ltuid': ltuid, 'ltoken': ltoken}
+ 
+r = requests.post(url, headers=headers, cookies=cookies)
+print(r.json()['message'])
